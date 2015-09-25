dc925ef36380ff3ae8b0d318df038195a4ae85c7
stuart <stu@reilly-family.co.uk>
2015-09-25 21:07:18 +0100

    Fixed missing table.js and font-awesome

---
+++ src/me/timetabler/ui/html/assets/js/table.js
@@ -0,0 +1,37 @@
+$('#staffModal').on('shown.bs.modal', function () {
+  $('#staffAddButton').focus()
+});
+$('#subjectModal').on('shown.bs.modal', function () {
+  $('#subjectAddButton').focus()
+});
+window.onload = function () {
+    brige.out("Loaded UI");
+}
+
+function addStaff(e) {
+    var id = document.getElementById("staffID").value;
+    var name = document.getElementById("staffName").value;
+    displayStaff(id, name);
+}
+
+function displayStaff(id, name) {
+    var table = document.getElementById("staffTable");
+    var row = table.insertRow(table.rows.length);
+
+    row.insertCell(0).innerHTML = id;
+    row.insertCell(1).innerHTML = name;
+}
+
+function addSubject(e) {
+    var id = document.getElementById("subjectID");
+    var name = document.getElementById("subjectName");
+    displaySubject(id, name);
+}
+
+function displaySubject(id, name) {
+    var table = document.getElementById("subjectTable");
+    var row = table.insertRow(table.rows.length);
+
+    row.insertCell(0).innerHTML = id;
+    row.insertCell(1).innerHTML = name;
+}
