c9bea01dad59f64957bedc30cea2f3d9c45b787e
stuart <stu@reilly-family.co.uk>
2016-02-26 07:26:25 +0000

    Remove db files for compatibility Write test strategy Add RuntimeExceptions to Database to catch SQLExceptions Change Database to use DataSource instead of DriverManager

---
+++ src/me/timetabler/sql/DatabaseUpdateException.java
@@ -0,0 +1,10 @@
+package me.timetabler.sql;
+
+/**
+ * Created by stuart on 25/02/16.
+ */
+public class DatabaseUpdateException extends DatabaseException {
+    public DatabaseUpdateException(Throwable cause) {
+        super("Failed to update database!" ,cause);
+    }
+}
